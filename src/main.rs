217ca4d85d556fa1a6fb219a19e59f41f4d42253
Adil Ahmed <adilahmeddev@gmail.com>
2023-11-14 20:06:06 +0000

    early steps of a tokeniser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,40 @@
-
 fn main() {
-    println!("Hello, world!");
+    let input = "{()}";
+
+    let mut output: String = String::default();
+    for c in input.chars().into_iter() {
+        output.push_str(
+            match c {
+                ')' => Token::RParen,
+                '{' => Token::LBrace,
+                '(' => Token::LParen,
+                '}' => Token::RBrace,
+                _ => Token::Nil,
+            }
+            .as_str(),
+        );
+    }
+
+    println!("{}",output)
+}
+
+enum Token {
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+
+    Nil,
+}
+
+impl Token {
+    fn as_str(&self) -> &str {
+        match self {
+            Token::LParen => "(",
+            Token::RParen => ")",
+            Token::LBrace => "{",
+            Token::RBrace => "}",
+            Token::Nil => "nil",
+        }
+    }
 }
