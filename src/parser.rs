557bc6ba5a13bacab536d1d273f739c56b6298ee
Adil Ahmed <adilahmeddev@gmail.com>
2023-12-23 10:22:59 +0000

    some slight progress

--- src/parser.rs
+++ src/parser.rs
@@ -10,18 +10,37 @@ struct Parser{
 
 impl Parser{
 
-    fn New(tokens: Vec<Token>)->Self{
-        
+    fn New(tokens: Vec<Token>, n: NodeType)->Self{
+        let _ = match n {
+            NodeType::Statement(s) =>{
+                s.token_literal()
+            }, 
+            NodeType::Expression => todo!(),
+        }
     }
 }
 
 enum NodeType{
-    Statement,
+    Statement(Statement),
     Expression,
 }
 
 trait Node:Display { 
-    fn TokenLiteral()->String;    
+    fn token_literal(&self)->String;    
 }
 
+struct Statement{
+}
 
+impl Node for Statement {
+    fn token_literal(&self)->String {
+        todo!()
+    }
+
+}
+
+impl Display for Statement {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
